4696343a1e905b1b72138c17f6c9c24e5a1cf656
shyamalpunekar <shyamal.punekar@gmail.com>
2018-07-07 13:02:55 -0700

    Add httpclient

--- app/src/main/java/com/restaurant/search/newbierob/YummlyService.java
+++ app/src/main/java/com/restaurant/search/newbierob/YummlyService.java
@@ -1,4 +1,11 @@
 package com.restaurant.search.newbierob;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
 
 public class YummlyService {
+    public static void findRecipes(String location, Callback callback) {
+        OkHttpClient client = new OkHttpClient.Builder()
+                .build();
+
+    }
 }
